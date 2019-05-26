cb8bff9ce2ed41315ea3c74c78fd7e1b759a0084
irinastoicheva <irinastoicheva@mail.bg>
2019-05-26 03:01:54 +0300

    commit

---
+++ JS Essentials/01. Syntax, Functions and Statements - Lab/10.WordsUppercase.js
@@ -0,0 +1,31 @@
+function solve(input) {
+    let arr = [];
+    let word = '';
+    let counter = -1;
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i].charCodeAt(0) >= 65 && input[i].charCodeAt(0) <= 90
+            || input[i].charCodeAt(0) >= 97 && input[i].charCodeAt(0) <= 122
+            || input[i].charCodeAt(0) == 95 || input[i].charCodeAt(0) >= 48 && input[i].charCodeAt(0) <= 57) {
+            if (input[i].charCodeAt(0) == 95 || input[i].charCodeAt(0) >= 48 && input[i].charCodeAt(0) <= 57){
+                word += input[i];
+            }
+            else {
+                word += input[i].toUpperCase();
+            }
+        }
+        else  {
+            if (word != '') {
+                counter++;
+                arr[counter] = word;
+                word = '';
+            }
+        }
+    }
+    if (word != '') {
+        arr[counter + 1] = word;
+    }
+            console.log(arr.join(', '));
+}
+
+solve('FUNCTIONS, IN, JS, CAN, BE, NESTED ,     I, E, HOLD, OTHER, FUNCTIONS');
