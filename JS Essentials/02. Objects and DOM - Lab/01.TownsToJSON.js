d9da63951bb53cc4a7c191a96077cb3590063da2
irinastoicheva <irinastoicheva@mail.bg>
2019-05-27 19:22:00 +0300

    commit

--- JS Essentials/02. Objects and DOM - Lab/01.TownsToJSON.js
+++ JS Essentials/02. Objects and DOM - Lab/01.TownsToJSON.js
@@ -1,5 +1,25 @@
-function solve() {
+function solve(arr) {
+    let output = [];
+    for (let i = 1; i < arr.length; i++) {
+        let input = arr[i].split(/[ ]*[|][ ]*/);
+        let token = [];
+        for (let j = 0; j < input.length; j++) {
+            if (input[j] != '') {
+                token.push(input[j]);
+            }
+        }
 
+        let obj = {
+            Town: token[0],
+            Latitude: Number(token[1]),
+            Longitude: Number(token[2])
+        }
+        output.push(obj);
+    }
+    console.log(JSON.stringify(output))
 }
 
-solve();
+solve(['| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |']
+);
