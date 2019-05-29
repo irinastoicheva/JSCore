4a39eeed3d18a313fe288ea96eeabb4eb3a17fd9
irinastoicheva <irinastoicheva@mail.bg>
2019-05-29 17:54:48 +0300

    commit

---
+++ JS Essentials/02. Objects and DOM - Exersice/01. Cards/solution.js
@@ -0,0 +1,21 @@
+function solve() {
+
+    let cards= Array.from( document.querySelectorAll(`img`))
+    let secondCards=document.getElementsByTagName(`img`);
+
+    console.log(cards);
+    cards.forEach(c=> c.addEventListener(`click`,showCard))
+
+
+
+ function showCard(e) {
+
+     console.log(e);
+     console.log(e.target);
+     let card= e.target;
+     card.setAttribute(`src`,`images/whiteCard.jpg`)
+     let attribute= card.getAttribute(`src`);
+     console.log(attribute);
+
+ }
+}
