005987b57d2cc2dd71c0024abfa33c93f8166e55
irinastoicheva <irinastoicheva@mail.bg>
2019-05-29 15:09:37 +0300

    commit

--- JS Essentials/02. Objects and DOM - Lab/07. Growing Word/solution.js
+++ JS Essentials/02. Objects and DOM - Lab/07. Growing Word/solution.js
@@ -1,4 +1,18 @@
 function growingWord() {
+    let text = document.querySelector('#exercise p');
+    let color = text.style.color;
+    switch (color) {
+        case '': text.style.color = 'blue'; break;
+        case 'blue': text.style.color = 'green'; break;
+        case 'green': text.style.color = 'red'; break;
+        case 'red': text.style.color = 'blue'; break;
+    }
 
-  //TODO...
+    let sizeArr = text.style.fontSize.split('px');
+    let size = Number(sizeArr[0]);
+    switch (size) {
+        case 0: text.style.fontSize = `${2}px`; break;
+        default:
+            text.style.fontSize = `${size * 2}px`; break;
+    }
 }
