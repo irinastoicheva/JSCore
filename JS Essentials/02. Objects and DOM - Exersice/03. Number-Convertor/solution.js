2c6dc3e8df6ca3fa45dd83ac6828be4db6a23eae
irinastoicheva <irinastoicheva@mail.bg>
2019-05-29 23:44:52 +0300

    commit

--- JS Essentials/02. Objects and DOM - Exersice/03. Number-Convertor/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/03. Number-Convertor/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    let menu = document.querySelector('#selectMenuTo');
 
-    //TODO...
+    let binaryOption = document.createElement('option');
+    binaryOption.text = 'Binary';
+    binaryOption.value = 'binary';
+    menu.add(binaryOption);
+
+    let hexOption = document.createElement('option');
+    hexOption.text = 'Hexadecimal';
+    hexOption.value = 'hexadecimal';
+    menu.add(hexOption);
+
+    let btn = document.getElementsByTagName('button')[0];
+    btn.addEventListener('click', () => {
+
+        let number = document.querySelector('#input');
+        let numberValue = Number(number.value);
+
+        let typeConvert = menu.value;
+
+        let result = 0;
+
+        if (typeConvert == 'binary') {
+            result = numberValue.toString(2);
+        } else if (typeConvert == 'hexadecimal') {
+            result = numberValue.toString(16).toUpperCase();
+        }
+
+        let outputField = document.querySelector('#result');
+        outputField.value = result;
+    });
+
 }
