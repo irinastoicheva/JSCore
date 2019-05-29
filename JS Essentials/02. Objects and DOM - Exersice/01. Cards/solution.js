616ef81a0bc9f3249ebf2954454f9c0f88ed1686
irinastoicheva <irinastoicheva@mail.bg>
2019-05-29 19:56:46 +0300

    commit

--- JS Essentials/02. Objects and DOM - Exersice/01. Cards/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/01. Cards/solution.js
@@ -1,7 +1,6 @@
 function solve() {
 
-    let cards= Array.from( document.querySelectorAll(`img`))
-    let secondCards=document.getElementsByTagName(`img`);
+    let cards= Array.from( document.querySelectorAll(`img`));
 
     console.log(cards);
     cards.forEach(c=> c.addEventListener(`click`,showCard))
@@ -13,9 +12,9 @@ function solve() {
      console.log(e);
      console.log(e.target);
      let card= e.target;
-     card.setAttribute(`src`,`images/whiteCard.jpg`)
-     let attribute= card.getAttribute(`src`);
-     console.log(attribute);
+     // card.setAttribute(`src`,`images/whiteCard.jpg`)
+     // let attribute= card.getAttribute(`src`);
+     // console.log(attribute);
 
  }
 }
--- JS Essentials/02. Objects and DOM - Exersice/02. Chat-Room/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/02. Chat-Room/solution.js
@@ -1,5 +1,14 @@
 function solve() {
-   //TODO...
+    let buttonSend = document.querySelector('#send');
+    buttonSend.addEventListener('click', () => {
+        let myOldMessage = document.getElementsByClassName('message my-message')[0];
+        let myCurrentMessage = myOldMessage.cloneNode(true);
+        let chatBoxFooter = document.querySelector('#chat_input');
+        let chatBoxFooterContent = chatBoxFooter.value;
+        myCurrentMessage.textContent = chatBoxFooterContent;
+        let divMessage = document.querySelector('#chat_messages');
+        divMessage.appendChild(myCurrentMessage);
+
+        chatBoxFooter.value = '';
+    });
 }
-
-
