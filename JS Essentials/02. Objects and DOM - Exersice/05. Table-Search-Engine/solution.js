96b5e74e2df403ad51f81c431433b65bfadea9ab
irinastoicheva <irinastoicheva@mail.bg>
2019-05-30 17:00:15 +0300

    commit

--- JS Essentials/02. Objects and DOM - Exersice/05. Table-Search-Engine/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/05. Table-Search-Engine/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-   //TODO...
+    let btn = document.querySelector('#searchBtn');
+    btn.addEventListener('click', () => {
+        let insertField = document.querySelector('#searchField');
+        let insertText = insertField.value.toLowerCase();
+        let tableBody = document.getElementsByTagName('tbody')[0];
+        let rowsInTableBody = tableBody.children;
+        for (let i = 0; i < rowsInTableBody.length; i++) {
+            let row = rowsInTableBody[i];
+            let cellInRow = row.children;
+            for (let j = 0; j < cellInRow.length; j++) {
+                let cell = cellInRow[j];
+                let cellText = cell.textContent.toLowerCase();
+                if (cellText.includes(insertText)) {
+                    row.classList.add('select');
+                    insertField.value = '';
+                }
+            }
+        }
+    });
 }
