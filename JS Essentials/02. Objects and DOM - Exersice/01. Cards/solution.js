ea1d1f6007063c2b048bb7b5f7325ac5ac697058
irinastoicheva <irinastoicheva@mail.bg>
2019-05-31 07:15:24 +0300

    commit

--- JS Essentials/02. Objects and DOM - Exersice/01. Cards/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/01. Cards/solution.js
@@ -1,20 +1,19 @@
 function solve() {
-
-    let cards= Array.from( document.querySelectorAll(`img`));
-
-    console.log(cards);
-    cards.forEach(c=> c.addEventListener(`click`,showCard))
-
-
-
- function showCard(e) {
-
-     console.log(e);
-     console.log(e.target);
-     let card= e.target;
-     // card.setAttribute(`src`,`images/whiteCard.jpg`)
-     // let attribute= card.getAttribute(`src`);
-     // console.log(attribute);
-
- }
+//    let cards= Array.from( document.querySelectorAll(`img`));
+//
+//    console.log(cards);
+//    cards.forEach(c=> c.addEventListener(`click`,showCard))
+//
+//
+//
+// function showCard(e) {
+//
+//     console.log(e);
+//     console.log(e.target);
+//     let card= e.target;
+//     // card.setAttribute(`src`,`images/whiteCard.jpg`)
+//     // let attribute= card.getAttribute(`src`);
+//     // console.log(attribute);
+//
+// }
 }
--- JS Essentials/02. Objects and DOM - Exersice/08. Numpad-Calculator/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/08. Numpad-Calculator/solution.js
@@ -1,4 +1,40 @@
 function solve() {
+    let keyButtons = document.getElementsByClassName('keys')[0].children;
+    let expressionOutputField = document.querySelector('#expressionOutput');
+    for (let i = 0; i < keyButtons.length; i++) {
+        let keyButton = keyButtons[i];
+        keyButton.addEventListener('click', () => {
+            switch (keyButton.textContent) {
+                case '+': expressionOutputField.textContent += ` ${keyButton.textContent} `; break;
+                case '-': expressionOutputField.textContent += ` ${keyButton.textContent} `; break;
+                case 'x': expressionOutputField.textContent += ` ${keyButton.textContent} `; break;
+                case '/': expressionOutputField.textContent += ` ${keyButton.textContent} `; break;
+                case '=':
+                    let text = expressionOutputField.textContent.split(' ');
+                    let result = 'NaN';
+                    let leftNumber = Number(text[0]);
+                    let operator = text[1];
+                    let rightNumber = Number(text[2]);
+                    if (rightNumber != ''){
+                        switch (operator) {
+                            case '+': result = leftNumber + rightNumber; break;
+                            case '-': result = leftNumber - rightNumber; break;
+                            case 'x': result = leftNumber * rightNumber; break;
+                            case '/': result = leftNumber / rightNumber; break;
+                        }
+                    }
 
-    //TODO...
+                    document.querySelector('#resultOutput').textContent = result;
+                    break;
+                default:
+                    expressionOutputField.textContent += keyButton.textContent;
+                    break;
+            }
+        });
+    }
+    let buttonClear = document.getElementsByClassName('clear')[0];
+    buttonClear.addEventListener('click', () => {
+        expressionOutputField.textContent = '';
+        document.querySelector('#resultOutput').textContent = '';
+    });
 }
