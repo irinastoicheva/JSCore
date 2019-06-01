d5f77f45c8ac0ef601d9f145b610fa19c98aeeec
irinastoicheva <irinastoicheva@mail.bg>
2019-06-01 13:57:48 +0300

    commit

--- JS Essentials/02. Objects and DOM - Exersice/09. Order-the-Names/solution.js
+++ JS Essentials/02. Objects and DOM - Exersice/09. Order-the-Names/solution.js
@@ -1,4 +1,26 @@
 function solve() {
-
-    //TODO...
+    let buttonAdd = document.getElementsByTagName('button')[0];
+    buttonAdd.addEventListener('click', () => {
+        let textInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+        if (textInput != '') {
+            let text = '';
+            for (let i = 0; i < textInput.length; i++) {
+                if (i == 0) {
+                    text += textInput[i].toUpperCase();
+                } else {
+                    text += textInput[i];
+                }
+            }
+            let list = document.getElementsByTagName('ol')[0];
+            let numberRow = textInput.charCodeAt(0) - 97;
+            console.log(numberRow);
+            let rows = list.children;
+            if (rows[+numberRow].textContent == '') {
+                rows[+numberRow].textContent = text;
+            } else {
+                rows[+numberRow].textContent += `, ${text}`;
+            }
+            document.getElementsByTagName('input')[0].value = '';
+        }
+    });
 }
