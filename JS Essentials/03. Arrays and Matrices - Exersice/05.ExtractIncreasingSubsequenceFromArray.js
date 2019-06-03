47a8aad427eab0bf99656a1490ce892586a94ced
irinastoicheva <irinastoicheva@mail.bg>
2019-06-03 07:01:06 +0300

    commit

---
+++ JS Essentials/03. Arrays and Matrices - Exersice/05.ExtractIncreasingSubsequenceFromArray.js
@@ -0,0 +1,25 @@
+function solve(arr) {
+    arr = arr.map(Number);
+    let result = [arr[0]];
+    let biggestNumber = [arr[0]];
+
+    for (let i = 1; i < arr.length; i++) {
+
+        let currentNumber =arr[i];
+
+        if (currentNumber >= biggestNumber) {
+            result.push(currentNumber);
+            biggestNumber = currentNumber;
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+
+solve([20,
+    3,
+    2,
+    15,
+    6,
+    1]
+);
