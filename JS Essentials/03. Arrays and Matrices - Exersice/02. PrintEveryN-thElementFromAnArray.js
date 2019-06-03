e274d1ee09f1d084eca7720bc8a741d2d6877aac
irinastoicheva <irinastoicheva@mail.bg>
2019-06-03 05:45:08 +0300

    commit

---
+++ JS Essentials/03. Arrays and Matrices - Exersice/02. PrintEveryN-thElementFromAnArray.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+let step = Number(arr.pop());
+let output = [];
+    for (let i = 0; i < arr.length ; i += step) {
+        output.push(arr[i]);
+    }
+    console.log(output.join('\n'));
+}
+solve(['dsa',
+    'asd',
+    'test',
+    'tset',
+    '2']
+);
