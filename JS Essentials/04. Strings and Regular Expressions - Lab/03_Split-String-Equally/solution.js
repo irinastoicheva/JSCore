a1b870d4476cbc92e4d2e78668d94ee234b81f59
irinastoicheva <irinastoicheva@mail.bg>
2019-06-06 20:11:55 +0300

    commit

--- JS Essentials/04. Strings and Regular Expressions - Lab/03_Split-String-Equally/solution.js
+++ JS Essentials/04. Strings and Regular Expressions - Lab/03_Split-String-Equally/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-    //TODO...
+    let text = document.querySelector('#text').value;
+    let number = Number(document.querySelector('#number').value);
+
+    if (text.length % number !== 0) {
+        text += text.substr(0, number - (text.length % number));
+    }
+
+    let result = [];
+
+    for (let i = 0; i < text.length; i += number) {
+        result.push(text.substr(i, number));
+    }
+    console.log(result);
+    let resultField = document.querySelector('#result');
+    resultField.textContent += result.join(' ');
 }
