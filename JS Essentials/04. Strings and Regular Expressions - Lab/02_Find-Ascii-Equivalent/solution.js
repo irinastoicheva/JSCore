b8f0c974407ec6f0f177401f1e3d6005a51a1b14
irinastoicheva <irinastoicheva@mail.bg>
2019-06-06 19:31:45 +0300

    commit

--- JS Essentials/04. Strings and Regular Expressions - Lab/02_Find-Ascii-Equivalent/solution.js
+++ JS Essentials/04. Strings and Regular Expressions - Lab/02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    let inputText = document.querySelector('#text').value.split(' ');
+    let numbers = [];
+    let words = [];
 
-  //TODO...
+    inputText.map(function (part) {
+        if (parseInt(part)) {
+            numbers.push(part);
+        } else {
+            words.push(part);
+        }
+    });
+
+    let resultField = document.querySelector('#result');
+
+    for (let i = 0; i < words.length; i++) {
+       words[i] = Array.from(words[i]).map(function(l){return l.charCodeAt(0)});
+       let p = document.createElement('p');
+       p.textContent = words[i].join(' ');
+       resultField.appendChild(p);
+    }
+
+    numbers = numbers.map(l => String.fromCharCode(l));
+    let p = document.createElement('p');
+    p.textContent = numbers.join('');
+    resultField.appendChild(p);
 }
