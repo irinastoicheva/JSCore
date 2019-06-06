9f78c391e6f82b4cd179541a2f8785a57e1ff008
irinastoicheva <irinastoicheva@mail.bg>
2019-06-06 20:57:54 +0300

    commit

--- JS Essentials/04. Strings and Regular Expressions - Lab/04_Replace-a-Certain-Word/solution.js
+++ JS Essentials/04. Strings and Regular Expressions - Lab/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+    let word = document.querySelector('#word').value;
+    let text = JSON.parse(document.querySelector('#text').value);
+    console.log(text);
 
-    //TODO...
+    let resultField = document.querySelector('#result');
+    let arr = text[0].split(' ');
+    let wordReplace = arr[2];
+    text.map(function (part) {
+        let pattern = new RegExp(wordReplace, 'ig');
+        part = part.replace(pattern, word);
+        let p = document.createElement('p');
+        p.textContent = part;
+        resultField.appendChild(p);
+        console.log(part);
+    });
+
+
 }
